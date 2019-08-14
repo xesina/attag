f9869e9f13c73abb4a29312057db657d33835a16
Sina Saeidi <xesina@gmail.com>
2019-08-15 04:29:20 +0430

    added basic authorization with jwt token usign ginjwt middleware

--- go.mod
+++ go.mod
@@ -4,26 +4,47 @@ go 1.12
 
 require (
 	cloud.google.com/go v0.44.0 // indirect
+	github.com/appleboy/gin-jwt/v2 v2.6.2 // indirect
+	github.com/astaxie/beego v1.12.0 // indirect
+	github.com/bradfitz/gomemcache v0.0.0-20190329173943-551aad21a668 // indirect
+	github.com/casbin/casbin v1.9.1 // indirect
+	github.com/couchbase/go-couchbase v0.0.0-20190808163554-a309852c6932 // indirect
+	github.com/couchbase/gomemcached v0.0.0-20190515232915-c4b4ca0eb21d // indirect
+	github.com/couchbase/goutils v0.0.0-20190315194238-f9d42b11473b // indirect
 	github.com/denisenkom/go-mssqldb v0.0.0-20190806190131-db2462fef53b // indirect
+	github.com/edsrzf/mmap-go v1.0.0 // indirect
 	github.com/gin-contrib/cors v1.3.0 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/gin-gonic/gin v1.4.0
+	github.com/go-redis/redis v6.15.2+incompatible // indirect
+	github.com/gogo/protobuf v1.2.1 // indirect
+	github.com/golang/snappy v0.0.1 // indirect
 	github.com/google/go-cmp v0.3.1 // indirect
 	github.com/google/pprof v0.0.0-20190723021845-34ac40c74b70 // indirect
+	github.com/gorilla/mux v1.7.3 // indirect
+	github.com/gorilla/pat v1.0.1 // indirect
 	github.com/hashicorp/golang-lru v0.5.3 // indirect
 	github.com/jinzhu/gorm v1.9.10
 	github.com/json-iterator/go v1.1.7 // indirect
+	github.com/kisielk/errcheck v1.2.0 // indirect
 	github.com/kr/pty v1.1.8 // indirect
+	github.com/labstack/gommon v0.2.9 // indirect
 	github.com/lib/pq v1.2.0 // indirect
-	github.com/mattn/go-isatty v0.0.8 // indirect
 	github.com/mattn/go-sqlite3 v1.11.0 // indirect
-	github.com/stretchr/objx v0.2.0 // indirect
+	github.com/onsi/ginkgo v1.8.0 // indirect
+	github.com/onsi/gomega v1.5.0 // indirect
+	github.com/pelletier/go-toml v1.4.0 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
+	github.com/siddontang/ledisdb v0.0.0-20190202134119-8ceb77e66a92 // indirect
+	github.com/syndtr/goleveldb v1.0.0 // indirect
+	github.com/tidwall/gjson v1.3.2 // indirect
 	github.com/ugorji/go v1.1.7 // indirect
+	github.com/wendal/errors v0.0.0-20181209125328-7f31f4b264ec // indirect
 	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4 // indirect
 	golang.org/x/mobile v0.0.0-20190806162312-597adff16ade // indirect
 	golang.org/x/net v0.0.0-20190813141303-74dc4d7220e7 // indirect
 	golang.org/x/sys v0.0.0-20190813064441-fde4db37ae7a // indirect
-	golang.org/x/tools v0.0.0-20190814171936-5b18234b3ae0 // indirect
+	golang.org/x/tools v0.0.0-20190814221113-9fae7b2cd541 // indirect
 	google.golang.org/grpc v1.22.1 // indirect
 	honnef.co/go/tools v0.0.1-2019.2.2 // indirect
 )
--- main.go
+++ main.go
@@ -6,11 +6,28 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"log"
 	"net/http"
 	"strings"
 	"time"
 	"github.com/gin-contrib/cors"
+	"github.com/appleboy/gin-jwt/v2"
 )
+
+// User represents a user
+type User struct {
+	ID        uint   `json:"id" gorm:"primary_key"`
+	Username  string `json:"name"`
+	Password  string
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+// login represents a login request
+type login struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
 
 // Bookmark represents a bookmark
 type Bookmark struct {
@@ -52,6 +69,11 @@ func main() {
 	r := gin.Default()
 	r.Use(cors.Default())
 
+	auth, err := authMiddleware()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	v1 := r.Group("/api/v1")
 
 	v1.GET("/ping", func(c *gin.Context) {
@@ -60,57 +82,146 @@ func main() {
 		})
 	})
 
-	v1.GET("/bookmarks", func(c *gin.Context) {
-		var bookmarks []Bookmark
-		if err := db.Find(&bookmarks).Error; err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"message": "internal server error"})
-		}
+	v1.POST("/login", auth.LoginHandler)
 
-		c.JSON(http.StatusOK, bookmarks)
+
+	r.NoRoute(auth.MiddlewareFunc(), func(c *gin.Context) {
+		claims := jwt.ExtractClaims(c)
+		log.Printf("NoRoute claims: %#v\n", claims)
+		c.JSON(404, gin.H{"code": "PAGE_NOT_FOUND", "message": "Page not found"})
 	})
 
-	v1.GET("/bookmarks/:id", func(c *gin.Context) {
-		id := c.Param("id")
-		var bookmark Bookmark
-		if err := db.First(&bookmark, "id = ?", id).Error; err != nil {
-			if gorm.IsRecordNotFoundError(err) {
-				c.JSON(http.StatusNotFound, gin.H{"message": "resource not found"})
+	// Refresh time can be longer than token timeout
+	v1.GET("/refresh_token", auth.RefreshHandler)
+
+	v1.Use(auth.MiddlewareFunc())
+	{
+		v1.GET("/bookmarks", func(c *gin.Context) {
+			var bookmarks []Bookmark
+			if err := db.Find(&bookmarks).Error; err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"message": "internal server error"})
+			}
+
+			c.JSON(http.StatusOK, bookmarks)
+		})
+
+		v1.GET("/bookmarks/:id", func(c *gin.Context) {
+			id := c.Param("id")
+			var bookmark Bookmark
+			if err := db.First(&bookmark, "id = ?", id).Error; err != nil {
+				if gorm.IsRecordNotFoundError(err) {
+					c.JSON(http.StatusNotFound, gin.H{"message": "resource not found"})
+					return
+				}
+				c.JSON(http.StatusInternalServerError, gin.H{"message": "internal server error"})
 				return
 			}
-			c.JSON(http.StatusInternalServerError, gin.H{"message": "internal server error"})
-			return
-		}
 
-		c.JSON(http.StatusOK, bookmark)
-	})
-
-	v1.POST("/bookmarks", func(c *gin.Context) {
-		var req Bookmark
-		if err := c.ShouldBindJSON(&req); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"message": "bad request"})
-		}
-
-		if err := db.Save(&req).Error; err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"message": "internal server error"})
-		}
-
-		c.JSON(http.StatusCreated, req)
-	})
-
-	v1.PUT("/bookmarks/:id", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "not implemented",
+			c.JSON(http.StatusOK, bookmark)
 		})
-	})
 
-	v1.DELETE("/bookmarks/:id", func(c *gin.Context) {
-		id := c.Param("id")
-		if err := db.Delete(&Bookmark{}, "id = ?", id).Error; err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"message": "internal server error"})
-		}
+		v1.POST("/bookmarks", func(c *gin.Context) {
+			var req Bookmark
+			if err := c.ShouldBindJSON(&req); err != nil {
+				c.JSON(http.StatusBadRequest, gin.H{"message": "bad request"})
+			}
 
-		c.JSON(http.StatusOK, gin.H{"message": "ok"})
-	})
+			if err := db.Save(&req).Error; err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"message": "internal server error"})
+			}
+
+			c.JSON(http.StatusCreated, req)
+		})
+
+		v1.PUT("/bookmarks/:id", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "not implemented",
+			})
+		})
+
+		v1.DELETE("/bookmarks/:id", func(c *gin.Context) {
+			id := c.Param("id")
+			if err := db.Delete(&Bookmark{}, "id = ?", id).Error; err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"message": "internal server error"})
+			}
+
+			c.JSON(http.StatusOK, gin.H{"message": "ok"})
+		})
+	}
 
 	r.Run("127.0.0.1:8585")
+}
+
+func authMiddleware() (*jwt.GinJWTMiddleware, error) {
+	identityKey := "id"
+	// the jwt middleware
+	authMiddleware, err := jwt.New(&jwt.GinJWTMiddleware{
+		Realm:       "attag",
+		Key:         []byte("secret key"),
+		Timeout:     time.Hour,
+		MaxRefresh:  time.Hour,
+		IdentityKey: identityKey,
+		PayloadFunc: func(data interface{}) jwt.MapClaims {
+			if v, ok := data.(*User); ok {
+				return jwt.MapClaims{
+					identityKey: v.Username,
+				}
+			}
+			return jwt.MapClaims{}
+		},
+		IdentityHandler: func(c *gin.Context) interface{} {
+			claims := jwt.ExtractClaims(c)
+			return &User{
+				Username: claims["id"].(string),
+			}
+		},
+		Authenticator: func(c *gin.Context) (interface{}, error) {
+			var loginVals login
+			if err := c.ShouldBindJSON(&loginVals); err != nil {
+				return "", jwt.ErrMissingLoginValues
+			}
+			userID := loginVals.Username
+			password := loginVals.Password
+
+			if (userID == "admin" && password == "admin") || (userID == "test" && password == "test") {
+				return &User{
+					Username:  userID,
+				}, nil
+			}
+
+			return nil, jwt.ErrFailedAuthentication
+		},
+		Authorizator: func(data interface{}, c *gin.Context) bool {
+			if v, ok := data.(*User); ok && v.Username == "admin" {
+				return true
+			}
+
+			return false
+		},
+		Unauthorized: func(c *gin.Context, code int, message string) {
+			c.JSON(code, gin.H{
+				"code":    code,
+				"message": message,
+			})
+		},
+		// TokenLookup is a string in the form of "<source>:<name>" that is used
+		// to extract token from the request.
+		// Optional. Default value "header:Authorization".
+		// Possible values:
+		// - "header:<name>"
+		// - "query:<name>"
+		// - "cookie:<name>"
+		// - "param:<name>"
+		TokenLookup: "header: Authorization, query: token, cookie: jwt",
+		// TokenLookup: "query:token",
+		// TokenLookup: "cookie:token",
+
+		// TokenHeadName is a string in the header. Default value is "Bearer"
+		TokenHeadName: "Bearer",
+
+		// TimeFunc provides the current time. You can override it to use another time value. This is useful for testing or if your server uses a different time zone than your tokens.
+		TimeFunc: time.Now,
+	})
+
+	return authMiddleware, err
 }
