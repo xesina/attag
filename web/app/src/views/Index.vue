cb20980734f28fc6806469656d25916190eea458
Sina Saeidi <xesina@gmail.com>
2019-09-02 04:46:19 +0430

    web: refactor bookmark list view to grid cards

--- web/app/src/views/Index.vue
+++ web/app/src/views/Index.vue
@@ -1,13 +1,50 @@
 <template>
     <v-container fluid>
-        <v-row justify="center">
-            <v-col cols="12" md="11">
-                <v-data-table
-                        :headers="headers"
-                        :items="bookmarks"
-                        :items-per-page="5"
-                        class="elevation-1"
-                ></v-data-table>
+        <v-row align="center" justify="center">
+            <v-col cols="11">
+                <v-row justify="start" align="center">
+                    <v-col
+                            cols="12"
+                            lg="3"
+                            sm="6"
+                            md="4"
+                            xs="1"
+                            xl="2"
+                            v-for="bookmark in bookmarks"
+                            :key="bookmarks.name"
+                    >
+                        <v-card>
+                            <v-card-title
+                                    class="fill-height align-end"
+                                    v-text="bookmark.name"
+                            ></v-card-title>
+                            <v-card-text v-text="bookmark.url"></v-card-text>
+
+                            <v-card-actions>
+                                <div class="flex-grow-1"></div>
+                                <v-btn icon>
+                                    <v-icon>mdi-share-variant</v-icon>
+                                </v-btn>
+
+                                <v-menu offset-y>
+                                    <template v-slot:activator="{ on }">
+                                        <v-btn icon medium v-on="on">
+                                            <v-avatar>
+                                                <v-icon>mdi-dots-vertical</v-icon>
+                                            </v-avatar>
+                                        </v-btn>
+                                    </template>
+                                    <v-list>
+                                        <v-list-item>
+                                            <v-list-item-title>item</v-list-item-title>
+                                        </v-list-item>
+                                    </v-list>
+                                </v-menu>
+
+                            </v-card-actions>
+                        </v-card>
+                    </v-col>
+                </v-row>
             </v-col>
         </v-row>
     </v-container>
